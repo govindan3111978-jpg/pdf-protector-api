438d6e92f25f30692b1f8ba973e043fd06c07d42
govindan3111978-jpg <govindan.3111978@gmail.com>
2026-02-10 07:33:53 +0530

    Update protect.js

--- api/protect.js
+++ api/protect.js
@@ -2,13 +2,25 @@ const { encryptPDF } = require('@pdfsmaller/pdf-encrypt-lite');
 const Busboy = require('busboy');
 
 module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // --- SECURITY BLOCK START ---
+    const allowedOrigin = "https://www.toolsrippo.com"; // REPLACE THIS with your actual Blogger URL
+    const origin = req.headers.origin;
+
+    // Only allow requests from your specific Blogger domain
+    if (origin === allowedOrigin) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', 'null');
+    }
+    
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+    // Handle Pre-flight request
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
+    // --- SECURITY BLOCK END ---
 
     if (req.method !== 'POST') {
         return res.status(405).send('Method Not Allowed');
@@ -18,11 +30,9 @@ module.exports = async (req, res) => {
         const busboy = Busboy({ headers: req.headers });
         let fileBuffer = null;
         let password = '';
-        let fileName = 'protected.pdf';
 
         busboy.on('file', (name, file, info) => {
             const chunks = [];
-            fileName = info.filename;
             file.on('data', (data) => chunks.push(data));
             file.on('end', () => { fileBuffer = Buffer.concat(chunks); });
         });
